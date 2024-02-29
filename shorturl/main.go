6f2a6bc38f63d189782e66b1f58e59fc3716190a
Amit Prajapati <amit.kumar@zybisys.com>
2024-02-29 10:39:22 +0530

    adding message

--- shorturl/main.go
+++ shorturl/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"sync"
 
@@ -25,6 +26,6 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello server"))
 	})
-
+	fmt.Println("Server running...")
 	http.ListenAndServe(":3000", r)
 }
